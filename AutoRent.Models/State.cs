d90da191a9b17a4493d2943c08edaa41fae3a339
ChinonsoIke <chinonsoikewelugo@gmail.com>
2022-09-04 13:35:28 +0100

    add State model

---
+++ AutoRent.Models/State.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace AutoRent.Models
+{
+    public class State
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string Id { get; set; }
+        public string Name { get; set; }
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public DateTime CreatedAt { get; set; }
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime UpdatedAt { get; set; }
+
+        // add List<Location> when Location model created
+    }
+}
