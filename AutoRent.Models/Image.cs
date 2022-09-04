e1456e8df5b4768470ce9ccac89c968a47d78d89
Drpampam <john.omotoye@decagon.dev>
2022-09-04 15:43:11 +0100

    Added Image model

---
+++ AutoRent.Models/Image.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace AutoRent.Models
+{
+    public class Image
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string Id { get; set; }
+        public string ImageUrl { get; set; }
+        public DateTime CreatedAt { get; set; }
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime UpdatedAt { get; set; }
+        public string CarId { get; set; }
+        //add car prop when car model is created
+    }
+}
