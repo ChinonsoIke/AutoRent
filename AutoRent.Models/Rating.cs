a369bad599a68946bd9a1a6bd9d910b4a983f454
ChinonsoIke <chinonsoikewelugo@gmail.com>
2022-09-04 21:25:22 +0100

    Add Rating model

---
+++ AutoRent.Models/Rating.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace AutoRent.Models
+{
+    public class Rating
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public int Stars { get; set; }
+        public string Review { get; set; }
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public DateTime CreatedAt { get; set; }
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime UpdatedAt { get; set; }
+
+        public string UserId { get; set; }
+        public User User { get; set; }
+        public string Carid { get; set; }
+        public Car Car { get; set; }
+    }
+}
