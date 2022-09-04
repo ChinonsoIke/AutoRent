8069cfdeb1ed2af46b5c97e9f7bd48170a115ba6
Drpampam <john.omotoye@decagon.dev>
2022-09-04 21:17:24 +0100

    Updated bookingId and UserId

--- AutoRent.Models/Payment.cs
+++ AutoRent.Models/Payment.cs
@@ -13,14 +13,14 @@ namespace AutoRent.Models
         public string Id { get; set; }
         public decimal Amount { get; set; }
         public PaymentOption Option;
-        public Status Status;        
+        public PaymentStatus Status;        
         public DateTime CreatedAt { get; set; }
         [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
         public DateTime UpdatedAt { get; set; }
 
-        public string UserId { get; set; }
+        public User UserId { get; set; }
         // add User prop when user model is created
-        public string BookingId { get; set; }
+        public Booking BookingId { get; set; }
         // add Booking prop when user model is created
     }
 }
