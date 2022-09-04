9e8bef3c19b1890cc0bc04b9bb26a1f4d50e42cf
Drpampam <93790572+Drpampam@users.noreply.github.com>
2022-09-04 17:41:08 +0100

    Update Post.cs
    Changes made

--- AutoRent.Models/Post.cs
+++ AutoRent.Models/Post.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Text;
+
 
 namespace AutoRent.Models
 {
@@ -10,11 +7,8 @@ namespace AutoRent.Models
     {
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public string Id { get; set; }
-        [MaxLength(100)]
         public string Title { get; set; }
-        [MaxLength(10)]
         public string Body { get; set; }
-        [MaxLength(100)]
         public string FeaturedImage { get; set; }
     }
 }
