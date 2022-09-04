8c9090c03b8a644768fcd92facf0a8dbdb1847b7
Drpampam <john.omotoye@decagon.dev>
2022-09-04 23:50:08 +0100

    added users dto

---
+++ AutoRent.Dtos/UserRequestCompletionDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutoRent.Dtos
+{
+    public class UserRequestCompletionDto
+    {
+        public string Id { get; set; }
+        public string HomeAddress { get; set; }
+        public string NextOfKin { get; set; }
+        public string AddressNextOfKin { get; set; }
+        public string ContactNextOfKin { get; set; }
+        public string DrivingLicenceImage { get; set; }
+        public string UserImage { get; set; }
+
+    }
+}
