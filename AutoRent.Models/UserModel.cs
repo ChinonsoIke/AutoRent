85b81a05f329657ad439706cac8ad39a59b2ecf9
Samuel Oluwatosin ONIYILO <masterarcher6321@gmail.com>
2022-09-04 19:12:46 +0100

    User Model
    Created a user model for our expected users

---
+++ AutoRent.Models/UserModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace AutoRent.Models
+{
+    public class UserModel
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string HomeAddress { get; set; }
+        public string NextofKin { get; set; }
+        public string ContactNextofKin { get; set; }
+        public string AddressNextofKin { get; set; }
+        public string DrivingLisenceImage { get; set; }
+        public string UserImage { get; set; }
+        public bool Verified { get; set; }
+        public bool CheckNextofKin { get; set; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public DateTime Created_at { get; set; }
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime Updated_at { get; set; }
+    }
+}
