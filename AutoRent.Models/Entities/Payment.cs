340213424aca622c898a245b5ccabe4ea77e4b74
Drpampam <john.omotoye@decagon.dev>
2022-09-04 13:52:19 +0100

    Added Payment model, enums for : option,status

---
+++ AutoRent.Models/Entities/Payment.cs
@@ -0,0 +1,26 @@
+﻿using AutoRent.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace AutoRent.Models.Entities
+{
+    public class Payment
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string Id { get; set; }
+        [Required]
+        public decimal Amount { get; set; }
+        public Option option;
+        public Status status;
+        public Booking _Booking { get; set; }
+        public Booking CreatedAt { get; set; }
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime UpdatedAt { get; set; }
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string UserId { get; set; }
+    }
+}
