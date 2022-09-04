e0f9602d4e0383fca27f876dcfa2e79f80e582b1
Drpampam <john.omotoye@decagon.dev>
2022-09-04 16:48:02 +0100

    Review Post model

--- AutoRent.Models/Post.cs
+++ AutoRent.Models/Post.cs
@@ -1,10 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace AutoRent.Models
 {
-    internal class Post
+    public class Post
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string Id { get; set; }
+        [MaxLength(100)]
+        public string Title { get; set; }
+        [MaxLength(10)]
+        public string Body { get; set; }
+        [MaxLength(100)]
+        public string FeaturedImage { get; set; }
     }
 }
