04ec11d4b5971e444e7c223dc98a786038ba5092
James Uchenna <jamesuchennachi@gmail.com>
2022-09-04 16:28:01 +0100

    made included booking properties

--- AutoRent.Models/Booking.cs
+++ AutoRent.Models/Booking.cs
@@ -1,10 +1,33 @@
-﻿using System;
+﻿using AutoRent.Models.Enums;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace AutoRent.Models
 {
-    internal class Booking
+    public class Booking
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string Id { get; set; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public DateTime PickUpDate { get; set; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime ReturnDate { get; set; }
+        public string PickUpLocationId { get; set; }
+        public string ReturnLocationId { get; set; }
+        public decimal PriceTotal { get; set; }
+        public Status Status { get; set; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public DateTime CreatedAt { get; set; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime UpdatedAt { get; set; }
+        public string CarId { get; set; }
+        public string UserId { get; set; }
+        
     }
 }
