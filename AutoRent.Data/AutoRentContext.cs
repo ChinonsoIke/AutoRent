5ab99a3a111459d6a83d668131d7f0af910ebb6e
Ezechekwubetech2022 <ezechekwubeinnocent@gmail.com>
2022-09-05 23:48:32 +0100

    add db context

---
+++ AutoRent.Data/AutoRentContext.cs
@@ -0,0 +1,26 @@
+﻿using AutoRent.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutoRent.Data
+{
+    public class AutoRentContext: DbContext 
+    {
+        public AutoRentContext(DbContextOptions<AutoRentContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<CarFeature> Features { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+        public DbSet<Location> Locations { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Rating> Ratings { get; set; } 
+        public DbSet<User> Users { get; set; }
+        public DbSet<Feature> Feature { get; set; }
+        public DbSet<Payment> Payment { get; set; }
+
+    }
+}
