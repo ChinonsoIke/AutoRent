2417373cadfdf04ff043f2800e575b9e2347903c
Drpampam <john.omotoye@decagon.dev>
2022-09-05 21:07:11 +0100

    created profile for post and booking

--- AutoRent.Models/User.cs
+++ AutoRent.Models/User.cs
@@ -11,7 +11,7 @@ namespace AutoRent.Models
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public string Id { get; set; }
-        public string FirstName { get; set; }
+        public string FirstName { get; set; } 
         public string LastName { get; set; }
         public string PhoneNumber { get; set; }
         public string Email { get; set; }
