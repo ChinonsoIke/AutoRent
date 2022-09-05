6e37c9c8ac93f9cd6a6acc19231f4463ae3f47d8
Ezechekwubetech2022 <ezechekwubeinnocent@gmail.com>
2022-09-06 00:08:32 +0100

    update db context

--- AutoRent.Data/AutoRentDbContext.cs
+++ AutoRent.Data/AutoRentDbContext.cs
@@ -6,21 +6,21 @@ using System.Text;
 
 namespace AutoRent.Data
 {
-    public class AutoRentContext: DbContext 
+    public class AutoRentDbContext : DbContext 
     {
-        public AutoRentContext(DbContextOptions<AutoRentContext> options) : base(options)
+        public AutoRentDbContext(DbContextOptions<AutoRentDbContext> options) : base(options)
         {
 
         }
         public DbSet<Car> Cars { get; set; }
-        public DbSet<CarFeature> Features { get; set; }
+        public DbSet<CarFeature> CarFeatures { get; set; }
         public DbSet<Booking> Bookings { get; set; }
         public DbSet<Location> Locations { get; set; }
         public DbSet<Post> Posts { get; set; }
         public DbSet<Rating> Ratings { get; set; } 
         public DbSet<User> Users { get; set; }
-        public DbSet<Feature> Feature { get; set; }
-        public DbSet<Payment> Payment { get; set; }
+        public DbSet<Feature> Features { get; set; }
+        public DbSet<Payment> Payments { get; set; }
 
     }
 }
