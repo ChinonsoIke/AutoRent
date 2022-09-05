300841a18a8078c06528559c2102885cb15897f3
ChinonsoIke <chinonsoikewelugo@gmail.com>
2022-09-06 00:22:23 +0100

    add generic repository interface and implementation

---
+++ AutoRent.Data/Repositories/GenericRepository.cs
@@ -0,0 +1,44 @@
+﻿using AutoRent.Data.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoRent.Data.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly AutoRentDbContext _dbContext;
+
+        public GenericRepository(AutoRentDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public Task AddAsync(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<T>> GetAllAsync(Expression<Func<T, bool>> expression, List<string> includes = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetAsync(Expression<Func<T, bool>> expression, List<string> includes = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(T item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
