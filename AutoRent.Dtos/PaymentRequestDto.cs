5089bd3552b74617f528c0686b5bad101474a81b
ChinonsoIke <chinonsoikewelugo@gmail.com>
2022-09-05 18:22:44 +0100

    create payment dtos

---
+++ AutoRent.Dtos/PaymentRequestDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutoRent.Dtos
+{
+    public class PaymentRequestDto
+    {
+        public decimal Amount { get; set; }
+        public string BookingId { get; set; }
+        public string UserId { get; set; }
+    }
+}
