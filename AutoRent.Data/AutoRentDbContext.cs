2a8fe0ca5fcac3954d505881abfd3ecb512f37be
ChinonsoIke <chinonsoikewelugo@gmail.com>
2022-09-07 05:51:35 +0100

    setup migration

--- AutoRent.Data/AutoRentDbContext.cs
+++ AutoRent.Data/AutoRentDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using AutoRent.Models;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -21,6 +22,18 @@ namespace AutoRent.Data
         public DbSet<User> Users { get; set; }
         public DbSet<Feature> Features { get; set; }
         public DbSet<Payment> Payments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Booking>().Property(p => p.PriceTotal).HasColumnType("decimal(18,4)");
+            modelBuilder.Entity<Car>().Property(p => p.RentPricePerDay).HasColumnType("decimal(18,4)");
+            modelBuilder.Entity<Payment>().Property(p => p.Amount).HasColumnType("decimal(18,4)");
+            modelBuilder.Entity<CarFeature>().HasKey(cf => new { cf.CarId, cf.FeatureId });
+            modelBuilder.Entity<Car>().Property(p => p.Images)
+                .HasConversion(
+                    v => JsonConvert.SerializeObject(v),
+                    v => JsonConvert.DeserializeObject<List<string>>(v));
+        }
 
     }
 }
---
+++ AutoRent.Data/Seeder.cs
@@ -0,0 +1,43 @@
+﻿using AutoRent.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoRent.Data
+{
+    public class Seeder
+    {
+        public static async Task SeedDb(AutoRentDbContext dbContext)
+        {
+            var baseDir = Directory.GetCurrentDirectory();
+
+            await dbContext.Database.EnsureCreatedAsync();
+
+            if (!dbContext.Users.Any())
+            {
+
+                var json = File.ReadAllText(Path.Combine(baseDir, "dbseed/users.json"));
+
+                var users = JsonConvert.DeserializeObject<List<User>>(json);
+
+                await dbContext.AddRangeAsync(users);
+                await dbContext.SaveChangesAsync();
+            }
+
+            if (!dbContext.Locations.Any())
+            {
+
+                var json = File.ReadAllText(Path.Combine(baseDir, "dbseed/locations.json"));
+
+                var locations = JsonConvert.DeserializeObject<List<Location>>(json);
+
+                await dbContext.AddRangeAsync(locations);
+                await dbContext.SaveChangesAsync();
+            }
+        }
+    }
+}
